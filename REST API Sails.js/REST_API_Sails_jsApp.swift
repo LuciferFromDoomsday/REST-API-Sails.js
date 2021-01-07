9a8ecc55cf8a5a7f7da37221d250f64ed4e4a46f
LuciferFromDoomsday <48617655+LuciferFromDoomsday@users.noreply.github.com>
2021-01-07 22:29:45 +0600

    Initial Commit

---
+++ REST API Sails.js/REST_API_Sails_jsApp.swift
@@ -0,0 +1,17 @@
+//
+//  REST_API_Sails_jsApp.swift
+//  REST API Sails.js
+//
+//  Created by admin on 1/7/21.
+//
+
+import SwiftUI
+
+@main
+struct REST_API_Sails_jsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
